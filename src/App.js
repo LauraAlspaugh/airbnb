31ba62562a2caa27279681df7fe97ca058d00132
Laura <lauraalspaugh2002@icloud.com>
2024-04-18 16:26:52 -0600

    adding props

--- src/App.js
+++ src/App.js
@@ -14,9 +14,24 @@ function App() {
       <p className='paragraph'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
 </p>
 <div className='card-line'>
-<Card/>
-<Card/>
-<Card/>
+<Card
+picture= "https://images.unsplash.com/photo-1713283699002-ac9462cedf0a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8"
+star= "5.0"
+description= "Interior Design Exhibit"
+price= "125"
+/>
+<Card
+picture= "https://images.unsplash.com/photo-1710170600429-0d6e04d2d96e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8"
+star= "4.0"
+description= "Travel Tour"
+price= "175"
+/>
+<Card
+picture= "https://images.unsplash.com/photo-1713188702328-1115b19bb465?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+star= "5.0"
+description= "Photography Exhibit"
+price= "150"
+/>
 </div>
     </div>
   );
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-export default function Card(){
+export default function Card(props){
+    console.log(props)
     return(
         <main className="card-profile">
-            <img className="card-image" alt="card" src="https://images.unsplash.com/photo-1713283365745-a727fb26c52f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8"/>
-            <h3><i className="mdi mdi-star star-icon"></i> 5.0 USA</h3>
-            <h3>Interior Design Exhibit</h3>
-            <h3>From $125/person</h3>
+            <img className="card-image" alt="card" src={props.picture}/>
+            <h3><i className="mdi mdi-star star-icon"></i> {props.star} USA</h3>
+            <h3>{props.description}</h3>
+            <h3>{props.price}/person</h3>
         </main>
     )
 }
